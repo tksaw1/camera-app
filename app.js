557e0ea57261fb5d02c865e9a0ed05c65bd0461b
tksaw1 <tksaw1@student.monash.edu>
2020-06-12 00:30:44 +0800

    properly undone

--- app.js
+++ app.js
@@ -29,16 +29,17 @@ cameraTrigger.onclick = function() {
     cameraOutput.classList.add("taken");
 };
 
-/*/ --- edit code
+/* --- edit code
 var link = document.getElementById('camera--output');
   link.setAttribute('download', 'MintyPaper.png');
-  link.setAttribute('href', canvas.toDataURL("image/png").replace("image/png", "image/octet-stream"));
+  link.setAttribute('href', canvas.toDataURL("image/png").replace("image/png", "image/octet-stream"));  // sth is wrong here
   link.click();
 
+
+// & sth is wrong with the below line of code
 var image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");  // here is the most important part because if you dont replace you will get a DOM 18 exception.
 window.location.href=image; // it will save locally
-
-*//----
+*/
 
 
 // Start the video stream when the window loads
