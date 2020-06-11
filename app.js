b1146d696647e554cba6af428c847bab81b043d1
tksaw1 <tksaw1@student.monash.edu>
2020-06-12 00:15:37 +0800

    changes undone
    The camera app stopped working

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ cameraTrigger.onclick = function() {
     cameraOutput.classList.add("taken");
 };
 
-// --- edit code
+/*/ --- edit code
 var link = document.getElementById('camera--output');
   link.setAttribute('download', 'MintyPaper.png');
   link.setAttribute('href', canvas.toDataURL("image/png").replace("image/png", "image/octet-stream"));
@@ -38,7 +38,7 @@ var link = document.getElementById('camera--output');
 var image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");  // here is the most important part because if you dont replace you will get a DOM 18 exception.
 window.location.href=image; // it will save locally
 
-//----
+*//----
 
 
 // Start the video stream when the window loads
