4c960b292f18e7c0fea3551cec02967d3e9f49a7
tksaw1 <tksaw1@student.monash.edu>
2020-06-12 00:11:26 +0800

    Change: with download feature

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 // Set constraints for the video stream
 var constraints = { video: { facingMode: "user" }, audio: false };
+
 // Define constants
 const cameraView = document.querySelector("#camera--view"),
     cameraOutput = document.querySelector("#camera--output"),
     cameraSensor = document.querySelector("#camera--sensor"),
     cameraTrigger = document.querySelector("#camera--trigger")
+
 // Access the device camera and stream to cameraView
 function cameraStart() {
     navigator.mediaDevices
@@ -17,6 +19,7 @@ function cameraStart() {
         console.error("Oops. Something is broken.", error);
     });
 }
+
 // Take a picture when cameraTrigger is tapped
 cameraTrigger.onclick = function() {
     cameraSensor.width = cameraView.videoWidth;
@@ -25,5 +28,18 @@ cameraTrigger.onclick = function() {
     cameraOutput.src = cameraSensor.toDataURL("image/webp");
     cameraOutput.classList.add("taken");
 };
+
+// --- edit code
+var link = document.getElementById('camera--output');
+  link.setAttribute('download', 'MintyPaper.png');
+  link.setAttribute('href', canvas.toDataURL("image/png").replace("image/png", "image/octet-stream"));
+  link.click();
+
+var image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");  // here is the most important part because if you dont replace you will get a DOM 18 exception.
+window.location.href=image; // it will save locally
+
+//----
+
+
 // Start the video stream when the window loads
 window.addEventListener("load", cameraStart, false);
